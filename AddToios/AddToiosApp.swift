b51d98a49ea54ca82f4fc9aafa5c36fa17941159
sanidhya <sanidhya09@gmail.com>
2022-05-31 17:19:37 +0530

    Initial Commit

---
+++ AddToios/AddToiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddToiosApp.swift
+//  AddToios
+//
+//  Created by Sanidhya Kumar on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AddToiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddToios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddToios
+//
+//  Created by Sanidhya Kumar on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
