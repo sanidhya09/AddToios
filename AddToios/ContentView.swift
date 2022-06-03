c096a5283d92375d65914e779f11ba67429627a8
sanidhya <sanidhya09@gmail.com>
2022-06-03 18:41:24 +0530

    initial commit sandy

--- AddToios/ContentView.swift
+++ AddToios/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import Flutter
+import FlutterPluginRegistrant
 
 struct ContentView: View {
+    @State private var isPresented = false
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Open Flutter Module") {
+                    self.isPresented = true
+        }.sheet(isPresented: $isPresented) {
+                    FontPicker()
+                }
+            
+    }
+    
+}
+struct FontPicker: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> FlutterViewController {
+        let flutterViewController = FlutterViewController(project: nil, nibName: nil, bundle: nil)
+
+        return flutterViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: FlutterViewController, context: Context) {
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
